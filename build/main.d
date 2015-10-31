6e1a580c691aa00e2e6cf94819023d2625186d68
meladroit <meladroit@users.noreply.github.com>
2015-10-31 14:19:42 -0700

    builds with 2.5 URL

--- build/main.d
+++ build/main.d
@@ -1,130 +1,130 @@
-main.o: c:/3ds/oot3dhax_installer/source/main.c \
- c:/devkitPro/libctru/include/3ds.h \
- c:/devkitPro/libctru/include/3ds/types.h \
- c:/devkitPro/libctru/include/3ds/svc.h \
- c:/devkitPro/libctru/include/3ds/srv.h \
- c:/devkitPro/libctru/include/3ds/linear.h \
- c:/devkitPro/libctru/include/3ds/vram.h \
- c:/devkitPro/libctru/include/3ds/os.h \
- c:/devkitPro/libctru/include/3ds/gfx.h \
- c:/devkitPro/libctru/include/3ds/services/gsp.h \
- c:/devkitPro/libctru/include/3ds/console.h \
- c:/devkitPro/libctru/include/3ds/util/utf.h \
- c:/devkitPro/libctru/include/3ds/services/ac.h \
- c:/devkitPro/libctru/include/3ds/services/am.h \
- c:/devkitPro/libctru/include/3ds/services/apt.h \
- c:/devkitPro/libctru/include/3ds/services/cam.h \
- c:/devkitPro/libctru/include/3ds/services/y2r.h \
- c:/devkitPro/libctru/include/3ds/services/cfgnor.h \
- c:/devkitPro/libctru/include/3ds/services/cfgu.h \
- c:/devkitPro/libctru/include/3ds/services/csnd.h \
- c:/devkitPro/libctru/include/3ds/services/fs.h \
- c:/devkitPro/libctru/include/3ds/services/hid.h \
- c:/devkitPro/libctru/include/3ds/services/irrst.h \
- c:/devkitPro/libctru/include/3ds/services/httpc.h \
- c:/devkitPro/libctru/include/3ds/services/ir.h \
- c:/devkitPro/libctru/include/3ds/services/ns.h \
- c:/devkitPro/libctru/include/3ds/services/pm.h \
- c:/devkitPro/libctru/include/3ds/services/ps.h \
- c:/devkitPro/libctru/include/3ds/services/ptm.h \
- c:/devkitPro/libctru/include/3ds/services/soc.h \
- c:/devkitPro/libctru/include/3ds/services/mic.h \
- c:/devkitPro/libctru/include/3ds/services/mvd.h \
- c:/devkitPro/libctru/include/3ds/services/news.h \
- c:/devkitPro/libctru/include/3ds/services/qtm.h \
- c:/devkitPro/libctru/include/3ds/services/hb.h \
- c:/devkitPro/libctru/include/3ds/gpu/gx.h \
- c:/devkitPro/libctru/include/3ds/gpu/gpu.h \
- c:/devkitPro/libctru/include/3ds/gpu/registers.h \
- c:/devkitPro/libctru/include/3ds/gpu/shbin.h \
- c:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
- c:/devkitPro/libctru/include/3ds/sdmc.h \
- c:/devkitPro/libctru/include/3ds/romfs.h \
- c:/3ds/oot3dhax_installer/source/filesystem.h \
- c:/3ds/oot3dhax_installer/source/firmware.h
+main.o: /home/thete/phase/oot3dhax_installer/source/main.c \
+ /home/thete/devkitPro/libctru/include/3ds.h \
+ /home/thete/devkitPro/libctru/include/3ds/types.h \
+ /home/thete/devkitPro/libctru/include/3ds/svc.h \
+ /home/thete/devkitPro/libctru/include/3ds/srv.h \
+ /home/thete/devkitPro/libctru/include/3ds/linear.h \
+ /home/thete/devkitPro/libctru/include/3ds/vram.h \
+ /home/thete/devkitPro/libctru/include/3ds/os.h \
+ /home/thete/devkitPro/libctru/include/3ds/gfx.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/gsp.h \
+ /home/thete/devkitPro/libctru/include/3ds/console.h \
+ /home/thete/devkitPro/libctru/include/3ds/util/utf.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/ac.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/am.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/apt.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/cam.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/y2r.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/cfgu.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/csnd.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/fs.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/hid.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/irrst.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/httpc.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/ir.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/ns.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/pm.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/ps.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/ptm.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/soc.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/mic.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/mvd.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/news.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/qtm.h \
+ /home/thete/devkitPro/libctru/include/3ds/services/hb.h \
+ /home/thete/devkitPro/libctru/include/3ds/gpu/gx.h \
+ /home/thete/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ /home/thete/devkitPro/libctru/include/3ds/gpu/registers.h \
+ /home/thete/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ /home/thete/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ /home/thete/devkitPro/libctru/include/3ds/sdmc.h \
+ /home/thete/devkitPro/libctru/include/3ds/romfs.h \
+ /home/thete/phase/oot3dhax_installer/source/filesystem.h \
+ /home/thete/phase/oot3dhax_installer/source/firmware.h
 
-c:/devkitPro/libctru/include/3ds.h:
+/home/thete/devkitPro/libctru/include/3ds.h:
 
-c:/devkitPro/libctru/include/3ds/types.h:
+/home/thete/devkitPro/libctru/include/3ds/types.h:
 
-c:/devkitPro/libctru/include/3ds/svc.h:
+/home/thete/devkitPro/libctru/include/3ds/svc.h:
 
-c:/devkitPro/libctru/include/3ds/srv.h:
+/home/thete/devkitPro/libctru/include/3ds/srv.h:
 
-c:/devkitPro/libctru/include/3ds/linear.h:
+/home/thete/devkitPro/libctru/include/3ds/linear.h:
 
-c:/devkitPro/libctru/include/3ds/vram.h:
+/home/thete/devkitPro/libctru/include/3ds/vram.h:
 
-c:/devkitPro/libctru/include/3ds/os.h:
+/home/thete/devkitPro/libctru/include/3ds/os.h:
 
-c:/devkitPro/libctru/include/3ds/gfx.h:
+/home/thete/devkitPro/libctru/include/3ds/gfx.h:
 
-c:/devkitPro/libctru/include/3ds/services/gsp.h:
+/home/thete/devkitPro/libctru/include/3ds/services/gsp.h:
 
-c:/devkitPro/libctru/include/3ds/console.h:
+/home/thete/devkitPro/libctru/include/3ds/console.h:
 
-c:/devkitPro/libctru/include/3ds/util/utf.h:
+/home/thete/devkitPro/libctru/include/3ds/util/utf.h:
 
-c:/devkitPro/libctru/include/3ds/services/ac.h:
+/home/thete/devkitPro/libctru/include/3ds/services/ac.h:
 
-c:/devkitPro/libctru/include/3ds/services/am.h:
+/home/thete/devkitPro/libctru/include/3ds/services/am.h:
 
-c:/devkitPro/libctru/include/3ds/services/apt.h:
+/home/thete/devkitPro/libctru/include/3ds/services/apt.h:
 
-c:/devkitPro/libctru/include/3ds/services/cam.h:
+/home/thete/devkitPro/libctru/include/3ds/services/cam.h:
 
-c:/devkitPro/libctru/include/3ds/services/y2r.h:
+/home/thete/devkitPro/libctru/include/3ds/services/y2r.h:
 
-c:/devkitPro/libctru/include/3ds/services/cfgnor.h:
+/home/thete/devkitPro/libctru/include/3ds/services/cfgnor.h:
 
-c:/devkitPro/libctru/include/3ds/services/cfgu.h:
+/home/thete/devkitPro/libctru/include/3ds/services/cfgu.h:
 
-c:/devkitPro/libctru/include/3ds/services/csnd.h:
+/home/thete/devkitPro/libctru/include/3ds/services/csnd.h:
 
-c:/devkitPro/libctru/include/3ds/services/fs.h:
+/home/thete/devkitPro/libctru/include/3ds/services/fs.h:
 
-c:/devkitPro/libctru/include/3ds/services/hid.h:
+/home/thete/devkitPro/libctru/include/3ds/services/hid.h:
 
-c:/devkitPro/libctru/include/3ds/services/irrst.h:
+/home/thete/devkitPro/libctru/include/3ds/services/irrst.h:
 
-c:/devkitPro/libctru/include/3ds/services/httpc.h:
+/home/thete/devkitPro/libctru/include/3ds/services/httpc.h:
 
-c:/devkitPro/libctru/include/3ds/services/ir.h:
+/home/thete/devkitPro/libctru/include/3ds/services/ir.h:
 
-c:/devkitPro/libctru/include/3ds/services/ns.h:
+/home/thete/devkitPro/libctru/include/3ds/services/ns.h:
 
-c:/devkitPro/libctru/include/3ds/services/pm.h:
+/home/thete/devkitPro/libctru/include/3ds/services/pm.h:
 
-c:/devkitPro/libctru/include/3ds/services/ps.h:
+/home/thete/devkitPro/libctru/include/3ds/services/ps.h:
 
-c:/devkitPro/libctru/include/3ds/services/ptm.h:
+/home/thete/devkitPro/libctru/include/3ds/services/ptm.h:
 
-c:/devkitPro/libctru/include/3ds/services/soc.h:
+/home/thete/devkitPro/libctru/include/3ds/services/soc.h:
 
-c:/devkitPro/libctru/include/3ds/services/mic.h:
+/home/thete/devkitPro/libctru/include/3ds/services/mic.h:
 
-c:/devkitPro/libctru/include/3ds/services/mvd.h:
+/home/thete/devkitPro/libctru/include/3ds/services/mvd.h:
 
-c:/devkitPro/libctru/include/3ds/services/news.h:
+/home/thete/devkitPro/libctru/include/3ds/services/news.h:
 
-c:/devkitPro/libctru/include/3ds/services/qtm.h:
+/home/thete/devkitPro/libctru/include/3ds/services/qtm.h:
 
-c:/devkitPro/libctru/include/3ds/services/hb.h:
+/home/thete/devkitPro/libctru/include/3ds/services/hb.h:
 
-c:/devkitPro/libctru/include/3ds/gpu/gx.h:
+/home/thete/devkitPro/libctru/include/3ds/gpu/gx.h:
 
-c:/devkitPro/libctru/include/3ds/gpu/gpu.h:
+/home/thete/devkitPro/libctru/include/3ds/gpu/gpu.h:
 
-c:/devkitPro/libctru/include/3ds/gpu/registers.h:
+/home/thete/devkitPro/libctru/include/3ds/gpu/registers.h:
 
-c:/devkitPro/libctru/include/3ds/gpu/shbin.h:
+/home/thete/devkitPro/libctru/include/3ds/gpu/shbin.h:
 
-c:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+/home/thete/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
 
-c:/devkitPro/libctru/include/3ds/sdmc.h:
+/home/thete/devkitPro/libctru/include/3ds/sdmc.h:
 
-c:/devkitPro/libctru/include/3ds/romfs.h:
+/home/thete/devkitPro/libctru/include/3ds/romfs.h:
 
-c:/3ds/oot3dhax_installer/source/filesystem.h:
+/home/thete/phase/oot3dhax_installer/source/filesystem.h:
 
-c:/3ds/oot3dhax_installer/source/firmware.h:
+/home/thete/phase/oot3dhax_installer/source/firmware.h:
